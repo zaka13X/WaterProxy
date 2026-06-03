1eb9c96102cf5aa4c559ed6c5e16097d12ccaa19
zaka13 <105675209+zaka13X@users.noreply.github.com>
2026-06-03 10:20:40 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ logging.set_level(logging.NONE);
 Object.assign(wisp.options, {
 	allow_udp_streams: false,
 	hostname_blacklist: [/example\.com/],
-	dns_servers: ["1.1.1.3", "1.0.0.3"],
+	dns_servers: ["9.9.9.9", "149.112.112.112"],
 });
 
 const fastify = Fastify({
